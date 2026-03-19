5705b14ff5b357f9342a0551c5711be7860bf03b
hiroki <hiroki_kobayashi1218@icloud.com>
2026-03-19 13:51:14 +0900

    Java基礎学習4.md

---
+++ Java/Final.java
@@ -0,0 +1,8 @@
+public class Final {
+    public static void main(String[] args) {
+        final int MAX = 100;
+        System.out.println(MAX);
+        MAX = 200;
+        System.out.println(MAX);
+    }
+}
---
+++ Java/TypeCast.java
@@ -0,0 +1,11 @@
+public class TypeCast {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+        System.out.println(b);
+
+        double c = 10.9;
+        int d = (int)c;
+        System.out.println(d);
+    }
+}
